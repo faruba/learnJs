96555c1118fd62b1e76efa83548c67e99c8c1ae3
zhaoshu <farubafaruba@126.com>
2016-12-09 17:41:04 +0800

    refactor v1

--- source/world.js
+++ source/world.js
@@ -5,30 +5,51 @@ var mapData = [
 	[true,  true, true,  true],
 	[false, true, true,  false],
 ];
+const height = mapData.length;
+const width = mapData[0].length;
+const Direction = { N:1,E:2,W:3,S:4 }
 
+function getDestination(currentPos, direction) {
+	switch(direction){
+		case Direction.N: return {x:currentPos.x, y:currentPos.y -1};
+		case Direction.S: return {x:currentPos.x, y:currentPos.y +1};
+		case Direction.W: return {x:currentPos.x -1, y:currentPos.y};
+		case Direction.E: return {x:currentPos.x +1, y:currentPos.y};
+	}	
+}
+function canMoveTo(pos){
+	return pos.x >=0 && pos.x < width 
+		&& pos.y >=0 && pos.y < height 
+		&& mapData[pos.y][pos.x];
+}
 
 function moveNorth(){
-	var destPosY = playerPosition.y - 1;
-	if(destPosY >=0 && mapData[destPosY][playerPosition.x]){
-		playerPosition.y = destPosY;
+	var pos = getDestination(playerPosition, Direction.N);
+	if(canMoveTo(pos)){
+		playerPosition.x = pos.x;
+		playerPosition.y = pos.y;
+		//思考为什么 playerPosition = pos 不可以?
 	}
 }
 function moveSouth(){
-	var destPosY = playerPosition.y + 1;
-	if(destPosY <= 3 && mapData[destPosY][playerPosition.x]){
-		playerPosition.y = destPosY;
+	var pos = getDestination(playerPosition, Direction.S);
+	if(canMoveTo(pos)){
+		playerPosition.x = pos.x;
+		playerPosition.y = pos.y;
 	}
 }
 function moveWest(){
-	var destPosX = playerPosition.x - 1;
-	if(destPosX >= 0 && mapData[playerPosition.y][destPosX]){
-		playerPosition.x = destPosX;
+	var pos = getDestination(playerPosition, Direction.W);
+	if(canMoveTo(pos)){
+		playerPosition.x = pos.x;
+		playerPosition.y = pos.y;
 	}
 }
 function moveEast(){
-	var destPosX = playerPosition.x + 1;
-	if(destPosX <= 3 && mapData[playerPosition.y][destPosX]){
-		playerPosition.x = destPosX;
+	var pos = getDestination(playerPosition, Direction.E);
+	if(canMoveTo(pos)){
+		playerPosition.x = pos.x;
+		playerPosition.y = pos.y;
 	}
 }
 
